6da814425b9f2eb41de6c5e7d5cd03348a7467c9
YifuZhang314 <75716630+YifuZhang314@users.noreply.github.com>
2026-09-17 10:45:35 +0100

    List workshops from newest to oldest

--- src/data/events.ts
+++ src/data/events.ts
@@ -12,14 +12,15 @@ export interface AcademicEvent {
 
 export const academicEvents: readonly AcademicEvent[] = [
   {
-    title: 'Operator approaches to dynamics – new connections',
-    shortName: 'OMDW01',
+    title:
+      '3rd Workshop on Gradient Flows for Sampling, Inference, and Learning',
     type: 'Workshop',
-    status: 'Attended',
-    dateLabel: '17–21 August 2026',
-    startDate: '2026-08-17',
-    location: 'Isaac Newton Institute, Cambridge',
-    url: 'https://www.newton.ac.uk/event/omdw01/',
+    status: 'Upcoming',
+    dateLabel: '23–25 November 2026',
+    startDate: '2026-11-23',
+    location: '170 Queen’s Gate, Imperial College London',
+    url: 'https://gradientflows.com/',
+    presentation: 'Poster presentation',
   },
   {
     title:
@@ -33,14 +34,13 @@ export const academicEvents: readonly AcademicEvent[] = [
     url: 'https://www.numerical.rl.ac.uk/events/numerical-linear-algebra-workshop/',
   },
   {
-    title:
-      '3rd Workshop on Gradient Flows for Sampling, Inference, and Learning',
+    title: 'Operator approaches to dynamics – new connections',
+    shortName: 'OMDW01',
     type: 'Workshop',
-    status: 'Upcoming',
-    dateLabel: '23–25 November 2026',
-    startDate: '2026-11-23',
-    location: '170 Queen’s Gate, Imperial College London',
-    url: 'https://gradientflows.com/',
-    presentation: 'Poster presentation',
+    status: 'Attended',
+    dateLabel: '17–21 August 2026',
+    startDate: '2026-08-17',
+    location: 'Isaac Newton Institute, Cambridge',
+    url: 'https://www.newton.ac.uk/event/omdw01/',
   },
 ];
